1bb725e58034e351515d49df38b0a239b4e60223
Brandon Aguirre <brag4918@colorado.edu>
2018-03-21 12:35:07 -0600

    Add virtual python env

---
+++ DJANGO_ENV/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/brandonaguirre/Projects/CSCI-4448-project/DJANGO_ENV/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
