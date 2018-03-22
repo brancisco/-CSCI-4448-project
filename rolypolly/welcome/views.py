602a541cb068cb7f4a9712969057f3b9070f7142
lauren-raddatz <lauren.raddatz@workday.com>
2018-03-21 20:45:28 -0600

    initial

--- rolypolly/welcome/views.py
+++ rolypolly/welcome/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django.template import Template, Context
 
 from django.http import HttpResponse
 from rolypolly.classes.User import *
 
 def index(request):
-	user = Participant()
-	user.setName("Brandon")
-	return HttpResponse("Welcome {}, this is our login / sign up page.".format(user.getName()))
+    user = Participant()
+    user.setName("Brandon")
+    return HttpResponse("Welcome {}, this is our login / sign up page.".format(user.getName()))
+    # fp = open('./templates/index.html')
+    # t = Template(fp.read())
+    # fp.close()
+    # html = t.render(Context({}))
+    # return HttpResponse(html)
