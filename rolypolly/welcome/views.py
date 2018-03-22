44abe37d99668ce9cbdb94a83f9f8aeb6639a264
Brandon Aguirre <brag4918@colorado.edu>
2018-03-21 21:59:52 -0600

    update welcome view

--- rolypolly/welcome/views.py
+++ rolypolly/welcome/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from rolypolly.classes.User import *
 
 def index(request):
 	user = Participant()
 	user.setName("Brandon")
-	return HttpResponse("Welcome {}, this is our login / sign up page.".format(user.getName()))
+	return render(request, 'welcome/welcome.html', {})
+	# return HttpResponse("Welcome {}, this is our login / sign up page.".format(user.getName()))
+
+def init_participant(request):
+	print("hello")
+	if request.method == "POST":
+		
+		render_to_response('welcome/welcome.html', RequestContext(request, {'params':HttpRequest.POST}))
+	return render(request, 'welcome/welcome.html', {})
