883349da1e8a196475223437921c93d2c3f7cb6d
Brandon Aguirre <brag4918@colorado.edu>
2018-04-09 16:35:47 -0600

    update minor things in test interface

--- rolypolly/welcome/urls.py
+++ rolypolly/welcome/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('init_participant', views.init_participant, name='index'),
 ]
--- rolypolly/welcome/views.py
+++ rolypolly/welcome/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.shortcuts import render_to_response
@@ -7,14 +7,9 @@ from django.template import RequestContext
 from rolypolly.classes.User import *
 
 def index(request):
-	user = Participant()
-	user.setName("Brandon")
-	return render(request, 'welcome/welcome.html', {})
-	# return HttpResponse("Welcome {}, this is our login / sign up page.".format(user.getName()))
-
-def init_participant(request):
-	print("hello")
+	
+	message = "No User"
 	if request.method == "POST":
-		
-		render_to_response('welcome/welcome.html', RequestContext(request, {'params':HttpRequest.POST}))
-	return render(request, 'welcome/welcome.html', {})
+		message = request.POST
+	return render(request, 'welcome/welcome.html', {'message': message})
+	# return HttpResponse("Welcome {}, this is our login / sign up page.".format(user.getName()))
