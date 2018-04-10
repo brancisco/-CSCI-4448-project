870b6744d83e472e323de7e04e582324aa2d4e25
Brandon Aguirre <brag4918@colorado.edu>
2018-04-10 11:12:55 -0600

    complete login / signup and add dash app

--- rolypolly/dash/views.py
+++ rolypolly/dash/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from rolypolly.classes.User import *
+from dash.models import User
 
-# Create your views here.
+def index(request):
+	if 'member_id' in request.session.keys():
+		user = User.objects.get(pk=request.session['member_id'])
+	else:
+		redirect('/login')
+	return render(request, 'dash/dash.html', {'username': user.username})
--- rolypolly/welcome/views.py
+++ rolypolly/welcome/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404, render
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from rolypolly.classes.User import *
+from dash.models import User
 
 def index(request):
 	
@@ -13,3 +14,45 @@ def index(request):
 		message = request.POST
 	return render(request, 'welcome/welcome.html', {'message': message})
 	# return HttpResponse("Welcome {}, this is our login / sign up page.".format(user.getName()))
+
+def signup(request):
+	user = None
+	message = ''
+	if request.method == "POST":
+		
+		user = User(first_name=request.POST['first_name'],
+			last_name=request.POST['last_name'],
+			username=request.POST['username'],
+			email=request.POST['email'],
+			password=request.POST['password']
+		)
+		try:
+			user.save()
+			return render(request, 'welcome/success.html', {})
+		except:
+			message = 'fail'
+
+	return render(request, 'welcome/signup.html', {'message': message})
+
+def login(request):
+	message = ''
+	if request.method == 'POST':
+		if 'member_id' not in request.session.keys():
+			try:
+				user = User.objects.get(username=request.POST['username'])
+				print(user.password)
+				if user.password == request.POST['password']:
+					message = 'Authenticated!'
+					request.session['member_id'] = user.id
+					return redirect('/dash')
+				else:
+					message = 'Not Authenticated.'
+			except:
+				message = 'No matching user'
+				user = None
+			return render(request, 'welcome/login.html', {'message': message})
+
+	if 'member_id' in request.session.keys():
+		return redirect('/dash')
+
+	
