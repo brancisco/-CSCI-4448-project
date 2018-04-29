00ec3c1c9c2e7280c407f50be332b45fef148fa0
Brandon Aguirre <brag4918@colorado.edu>
2018-04-29 17:39:08 -0600

    SORRY, add many updates

--- rolypolly/dash/models.py
+++ rolypolly/dash/models.py
@@ -23,6 +23,7 @@ class Answer(models.Model):
 class Result(models.Model):
 	poll 			= models.ForeignKey('Poll', on_delete=models.CASCADE)
 	date_created 	= models.DateTimeField(auto_now_add=True)
-	date_closed 	= models.DateTimeField(auto_now_add=False)
+	date_closed 	= models.DateTimeField(auto_now_add=False, null=True)
 	code 			= models.CharField(max_length=5)
 	active_question = models.SmallIntegerField()
+	host 			= models.ForeignKey('User', on_delete=models.CASCADE)
--- rolypolly/take/views.py
+++ rolypolly/take/views.py
@@ -13,6 +13,7 @@ from take.models import Response
 
 
 def index(request):
+
     poll_code = request.session.get('poll_code')
     pollName = ''
     err = ''
@@ -21,27 +22,60 @@ def index(request):
     response = ''
     try:
         result = Result.objects.get(code = poll_code)
+        if result.date_closed:
+            request.session.clear()
+            return redirect('/welcome')
         poll = Poll.objects.get(pk = result.poll.id)
         pollName = poll.name
         quest = Question.objects.get(poll = result.poll.id, order = result.active_question)
         question = quest.text
         ans = Answer.objects.filter(question = quest.id)
-        # answer = [a.text for a in ans]
-        # ids = [a.id for a in ans]
+
     except:
         err = 'No question to answer ¯\_(ツ)_/¯'
         return render(request, 'take/takePoll.html', {'pollName':pollName, 'err': err})
 
     if request.method == "POST":
-        try:
-            oid = request.POST.get('answer')
-            r = Response(result_id=result.id, question_id=quest.id, answer_id=oid)
-            r.save()
-            return render(request, 'take/waitPage.html', {'pollName':pollName, 'poll_code':poll_code, 'response':oid})
-        except:
-            print("fail")
-            return JsonResponse({'success': False})
-    return render(request, 'take/takePoll.html', {'pollName':pollName, 'question':question, 'answer':ans})
+        # try:
+        aid = request.POST.get('answer')
+        print(request.session['answered'])
+        request.session['answered'][str(quest.id)] = True
+        request.session.modified = True
+        r = Response(result_id=result.id, question_id=quest.id, answer_id=aid)
+        r.save()
+        return render(request, 'take/waitPage.html', {'poll':poll, 'poll_code':poll_code, 'response':aid})
+        # except:
+        #     return JsonResponse({'success': False})
+    else:
+        if str(quest.id) in request.session['answered']:
+            return render(request, 'take/waitPage.html', {'poll':poll, 'poll_code':poll_code})
 
+    return render(request, 'take/takePoll.html', {'poll':poll, 'question':question, 'answer':ans})
+
+@csrf_exempt
 def wait(request):
-    print("We are now waiting")
+    if request.method == "POST" and request.session.get('started'):
+        poll_code = request.session.get('poll_code')
+        poll_id = request.session.get('poll_id')
+        qoid = request.session.get('qoid')
+        check = request.POST.get('check')
+        print(poll_code)
+        active_question = Result.objects.get(code=poll_code).active_question
+        is_closed = Result.objects.get(code=poll_code).date_closed
+        if is_closed:
+            request.session.clear()
+            request.session['thanks'] = True
+            return JsonResponse({'success': True, 'FINISH': True})
+
+        if int(active_question) != int(qoid):
+            request.session['qoid'] = active_question
+            return JsonResponse({'success': True, 'NEXTQ': True})
+
+        if int(check) > 300:
+            request.session.clear()
+            return JsonResponse({'success': False, 'CHECK_NUMBER': 'TIMED_OUT'})
+
+        return JsonResponse({'success': True, 'CHECK_NUMBER': int(check), 'qoid': int(qoid)})
+    else:
+        request.session.clear()
+        return JsonResponse({'success': False, 'CHECK_NUMBER': 'TIMED_OUT'})
