e9b984b20eaf6d0748f0aba77c6fc0ce897c72b2
lauren-raddatz <lauren.raddatz@workday.com>
2018-04-29 20:35:47 -0600

    clean result

--- rolypolly/rolypolly/classes/Result.py
+++ rolypolly/rolypolly/classes/Result.py
@@ -3,31 +3,35 @@
 
 from take.models import Response
 from dash.models import *
-# from TextInterface import TextInterface
 
 class ResultClass(TextInterface):
     def __init__(self):
         pass
+    
+    def getResult(self,):
+        user = User.objects.get(pk=request.session['member_id'])
+        reutrn Result.objects.all().filter(host=user).order_by('-date_created')
 
     def getPollCode(self,):
         poll_code = request.session.get('poll_code')
         return Result.objects.get(code = poll_code).code
+        
 
     def getQuestion(self,):
         # return Result.objects.get(poll=result.poll.id, order=result.active_question)
+        result = this.getResult()
         return Question.objects.get(poll=result.poll.id, order=result.active_question)
     
     def getAnswer(self,):
         # return Result.objects.get(question=quest.id)
+        quest = this.getQuestion()
         return Answer.objects.filter(question=quest.id)
         
     def getResponse(self,):
+        poll_code = this.getPollCode()
         return Result.objects.get(code=poll_code)
 
     def setResponse(self,):
+        result = this.getResult()
         result.poll = 1
         result.save()
-    
-    # dont need db pull
-    # just getters and setters for all the things in the model
-
